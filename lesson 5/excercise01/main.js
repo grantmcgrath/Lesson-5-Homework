96c4332dedfa88ff2a65297cfae26016a53b26e8
grantmcgrath <grantmcgrath@me.com>
2017-06-07 16:19:57 -0400

    Lesson 5, Exercises 1 - 4

---
+++ lesson 5/excercise01/main.js
@@ -0,0 +1,8 @@
+function addNumbers(numberA, numberB) {
+  console.log(numberA + numberB);
+  return numberA + numberB;
+}
+
+var twoPlusTwo = addNumbers(2, 2)
+
+console.log(twoPlusTwo) // 4
---
+++ lesson 5/excercise03/main.js
@@ -0,0 +1,7 @@
+function sentence(a, b, c) {
+  console.log("Param a is " + a);
+  console.log("Param b is " + b);
+  console.log("Param c is " + c);
+}
+
+announceParams("root beer", 24, "3.40");
