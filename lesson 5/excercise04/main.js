392f5aa6a1b3fa356d7953198cf9aa69c03de9fa
grantmcgrath <grantmcgrath@me.com>
2017-06-07 19:20:06 -0400

    Rewrote in Class
    We rewrote this exercise in class

--- lesson 5/excercise04/main.js
+++ lesson 5/excercise04/main.js
@@ -1,1 +1,14 @@
 //Write a function called isVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel, uppercase or lowercase. The function should return false if the character is not a vowel.
+
+function isVowel(str) {
+  var checkArray = ["a", "e", "i", "o", "u"];
+  for(var indexChecker = 0; indexChecker < checkArray.length; indexChecker++){
+    if (str === checkArray[indexChecker]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(isVowel("e"));
+console.log(isVowel("z"));
