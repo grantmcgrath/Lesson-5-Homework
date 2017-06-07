c8c2a56c712f82f874ce29087000ac1aaf9ffb73
grantmcgrath <grantmcgrath@me.com>
2017-06-07 18:56:26 -0400

    fixing exercise 3

--- lesson 5/excercise03/main.js
+++ lesson 5/excercise03/main.js
@@ -1,5 +1,16 @@
 //Write a function called longest that returns the longest of two input strings or arrays.
 
-function longest() {
-  return
+function longest(a, b) {
+  if (a.length>b.length) {
+    return a;
+  }
+  else {
+    return b;
+  }
 }
+
+var array1 = "bob, john, tod";
+var array2 = "pizza, bacon, tacos";
+var arrayLongest = longest(array1, array2);
+
+console.log(arrayLongest);
